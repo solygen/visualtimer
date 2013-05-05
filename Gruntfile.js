656cbc316db6083ad0db99338d534250d820b033
Frank Paczynski <frank.paczynski@solygen.de>
2013-05-05 19:23:28 +0200

    grunt basic

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  // Initializes the Grunt tasks with the following settings
+  grunt.initConfig({
+
+    // A list of files, which will be syntax-checked by JSHint
+jshint: {
+  files: ['Gruntfile.js', 'jq-vt.js']
+},
+
+    // Files to be concatenated (source and destination files)
+    concat: { 
+      js: {
+    src: ['jq-vt.js'],
+    dest: 'jq-vt-conc.js'
+  } },
+
+    //and minified (source and destination files)
+    uglify: { 
+     dist: {
+        src: ['<%= concat.js.dest %>'],
+        dest: 'jq-vt-conc.min.js'
+      }
+     },
+
+watch: {
+  files: '<%= jshint.files %>',
+  tasks: 'jshint'
+}
+
+
+
+  });
+
+  // Load the plugins that provide the tasks we specified in package.json.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+  // This is the default task being executed if Grunt
+  // is called without any further parameter.
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
+};
---
+++ jq-vt.js
@@ -0,0 +1,134 @@
+var t;
+
+//'use strict';
+
+var SECOND = 1000,
+    MINUTE = SECOND * 60;
+
+    timer = function() {
+
+        /**
+         * variables
+         */
+        var self = {},
+            running,
+            parent,
+            next,
+            interval;
+
+        /**
+         * task runner
+         */
+        var intervalId = setInterval(function() {
+
+            if (self.isRunning() && isNext()) {
+                //remove node
+                var divs = $('body').find('div'),
+                    node = $(divs[divs.length-1]);
+                if (divs.length !== 0)
+                    node.fadeOut(400, function() { node.remove(); } );
+                else {
+                    clearInterval(intervalId);
+                }
+            }
+        }, SECOND/10);
+
+        /**
+         * get current timestamp
+         * @return {number} timestamp
+         */
+        now = function () {
+            return (new Date()).getTime();
+        };
+
+        /**
+         * execute next step
+         * @return {boolean}
+         */
+        isNext = function() {
+            //console.log(next, now(), next - now())
+            var is = ( next || now() ) <= now();
+            if (is)
+                next = now() + interval;
+            return is;
+        };
+
+
+        /**
+         * get one dot node
+         * @param  {numeric} id
+         * @return {jquery}
+         */
+        dot = function (id) {
+            return $('<div>')
+                    .attr('id', id);
+        };
+
+        /**
+         * append timer to node
+         * @param  {number} time in milliseconds
+         * @param  {jquery} node
+         * @return {object} timer
+         */
+        self.init = function(time, node) {
+            parent = node || $('body');
+            time = time || MINUTE;
+            interval = SECOND;
+            var nodes = [];
+
+            //viewport
+            var rows = $(window).height() / 95;
+            var cols = $(window).width() / 95;
+            var count = Math.round(rows * cols);
+
+            //get interval to remove last
+            //dot when countdown ends
+            interval = time/count;
+
+            //add nodes
+            for (var i = count; i >= 0; i--) {
+                nodes.push(dot(i));
+            }
+            parent.append(nodes);
+            return this;
+        };
+
+        /**
+         * @return {Boolean}
+         */
+        self.isRunning = function() {
+            return running;
+        };
+
+        /**
+         * @return timer
+         */
+        self.start = function() {
+            running = true;
+            return this;
+        };
+
+        /**
+         * @return timer
+         */
+        self.pause = function() {
+            running = false;
+            return this;
+        };
+
+        /**
+         * @return timer
+         */
+        self.reset = function() {
+            return self.init(parent);
+        };
+
+        return self;
+    };
+
+//onready
+$(window).load(function() {
+console.log($('body').height());
+    t = timer().init(10 * SECOND).start();
+    //t.pause();
+});
